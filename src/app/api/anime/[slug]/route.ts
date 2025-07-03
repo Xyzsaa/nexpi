078428ad654c5cc5dd4178c4b6a8c7df6c6e8a0a
Xyzsaa <114806649+Xyzsaa@users.noreply.github.com>
2025-07-03 11:27:51 +0700

    Update route.ts

--- src/app/api/anime/[slug]/route.ts
+++ src/app/api/anime/[slug]/route.ts
@@ -1,12 +1,12 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server"
 import anime from "@/utils/anime";
 
-export async function GET(request: NextRequest, { params }: { params: { slug: string } }) {
+export async function GET(request: NextRequest, props: { params: Promise<{ slug: string }> }) {
+  const params = await props.params;
   try {
-    const data = await anime(params.slug);
-    return NextResponse.json({ data }, { status: 200 });
+    const data = await anime(params.slug)
+    return NextResponse.json({ data: data }, { status: 200 })
   } catch (error) {
-    console.error("Error fetching anime:", error);
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
   }
 }
