4595aaececb43d574751bee66ec3b555059120ee
Xyzsaa <114806649+Xyzsaa@users.noreply.github.com>
2025-07-03 11:26:39 +0700

    Update route.ts

--- src/app/api/anime/[slug]/route.ts
+++ src/app/api/anime/[slug]/route.ts
@@ -1,11 +1,9 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import anime from "@/utils/anime";
 
-export async function GET(request: NextRequest, context: { params: { slug: string } }) {
+export async function GET(request: NextRequest, { params }: { params: { slug: string } }) {
   try {
-    const slug = context.params.slug;
-    const data = await anime(slug);
-
+    const data = await anime(params.slug);
     return NextResponse.json({ data }, { status: 200 });
   } catch (error) {
     console.error("Error fetching anime:", error);
